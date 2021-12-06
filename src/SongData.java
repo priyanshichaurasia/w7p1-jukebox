42f0a8a2316b90df08a8bbf72a64216e1dd5d72a
priyanshic83@gmail.com <priyanshic83@gmail.com>
2021-12-06 15:04:13 +0530

    commit 4

---
+++ src/SongData.java
@@ -0,0 +1,66 @@
+public class SongData {
+    private int sId;
+    private String sName, timeDuration, artName, albName, gName;
+
+    public SongData(int sId, String sName, String timeDuration, String artName, String albName, String gName){
+        this.sId=sId;
+        this.sName=sName;
+        this.timeDuration=timeDuration;
+        this.artName=artName;
+        this.albName=albName;
+        this.gName=gName;
+    }
+
+    public int getsId() {
+        return sId;
+    }
+
+    public void setsId(int sId) {
+        this.sId = sId;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+
+    public String getTimeDuration() {
+        return timeDuration;
+    }
+
+    public void setTimeDuration(String timeDuration) {
+        this.timeDuration = timeDuration;
+    }
+
+    public String getArtName() {
+        return artName;
+    }
+
+    public void setArtName(String artName) {
+        this.artName = artName;
+    }
+
+    public String getAlbName() {
+        return albName;
+    }
+
+    public void setAlbName(String albName) {
+        this.albName = albName;
+    }
+
+    public String getgName() {
+        return gName;
+    }
+
+    public void setgName(String gName) {
+        this.gName = gName;
+    }
+    
+    @Override
+    public String toString(){
+        return (sId +" "+ sName +" "+ timeDuration +" "+ artName +" "+ albName +" "+ gName);
+    }
+}
